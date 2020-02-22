7334fee597bc3206d5899d05f4bdc8eb770ed41c
Nikita Y <nikitosnikn@yandex.ru>
2020-02-23 00:39:00 +0700

    Added exceptions

--- aioamocrm/exceptions.py
+++ aioamocrm/exceptions.py
@@ -1,19 +1,27 @@
 from .base import _BaseException
 
 __all__ = ['AmoAuthError', 'AmoPageNotFoundError', 'AmoInternalError', 'AmoObjectNotFoundError', 'AmoBadRequest',
-           'AmoUnknownError']
+           'AmoUnknownError', 'AmoPaymentRequired', 'AmoForbidden']
 
 
 class AmoAuthError(_BaseException):
-    message = 'Invalid credentials'
+    message = '№401. Invalid credentials or account has no permissions'
+
+
+class AmoPaymentRequired(_BaseException):
+    message = '№402. Payment Required'
+
+
+class AmoForbidden(_BaseException):
+    message = '№403. IP was banned or account was blocked'
 
 
 class AmoPageNotFoundError(_BaseException):
-    message = '404. Page not found'
+    message = '№404. Page not found'
 
 
 class AmoInternalError(_BaseException):
-    message = '500. AmoCRM Internal Error'
+    message = '№500. AmoCRM Internal Error'
 
 
 class AmoObjectNotFoundError(_BaseException):
--- aioamocrm/session.py
+++ aioamocrm/session.py
@@ -1,10 +1,9 @@
-import json
 from typing import Union
 from datetime import timedelta, datetime
 
 from httpx import Client
 
-from .exceptions import AmoAuthError, AmoPageNotFoundError, AmoInternalError, AmoBadRequest, AmoUnknownError
+from .exceptions import *
 
 __all__ = ['Session', 'SESSION_LIFETIME']
 
@@ -15,7 +14,7 @@ class Session:
     _amo_login: str = None
     _amo_hash: str = None
     _subdomain: str = None
-    _cookies = None
+    _cookies: dict = None
     _cookies_last_update: datetime = None
 
     def __init__(self, amo_login: str, amo_hash: str, subdomain: str):
@@ -25,12 +24,12 @@ class Session:
 
     @classmethod
     def init(cls, amo_login: str, amo_hash: str, subdomain: str):
-        cls._get_cookies(cls, amo_login, amo_hash, subdomain)
+        cls._authorize(cls, amo_login, amo_hash, subdomain)
         return cls(amo_login, amo_hash, subdomain)
 
     @property
     def auth_url(self) -> str:
-        return "https://{}.amocrm.ru/private/api/auth.php"
+        return f"https://{self.subdomain}.amocrm.ru/private/api/auth.php"
 
     @property
     def api_url(self) -> str:
@@ -43,17 +42,17 @@ class Session:
     @property
     def cookies(self) -> Union[dict, None]:
         if self._cookies and self._cookies_last_update + SESSION_LIFETIME < datetime.now():
-            return self._get_cookies(self._amo_login, self._amo_hash, self._subdomain)
+            return self._authorize()
 
         return self._cookies
 
-    def _get_cookies(self, amo_login: str, amo_hash: str, subdomain: str) -> None:
+    def _authorize(self, amo_login: str = None, amo_hash: str = None, subdomain: str = None) -> None:
         with Client() as session:
             resp = session.post(
-                self.auth_url.format(subdomain),
+                subdomain or self.auth_url,
                 data={
-                    'USER_LOGIN': amo_login,
-                    'USER_HASH': amo_hash,
+                    'USER_LOGIN': amo_login or self._amo_login,
+                    'USER_HASH': amo_hash or self._amo_hash,
                 })
 
         if resp.status_code == 200 and 'true' in resp.text:
@@ -62,11 +61,7 @@ class Session:
         else:
             raise AmoAuthError
 
-    def request(self, method: str, url: str, payload: dict = None, headers: dict = None) -> Union[dict, None]:
-        header = {}
-        if headers:
-            header.update(**headers)
-
+    def request(self, method: str, url: str, payload: dict = None) -> Union[dict, None]:
         if payload:
             payload = {'params': payload} if method.lower() == 'get' else {'data': payload}
         else:
@@ -81,6 +76,10 @@ class Session:
                 raise AmoBadRequest
             elif resp.status_code == 401:
                 raise AmoAuthError
+            elif resp.status_code == 402:
+                raise AmoPaymentRequired
+            elif resp.status_code == 403:
+                raise AmoForbidden
             elif resp.status_code == 404:
                 raise AmoPageNotFoundError
             elif resp.status_code == 500:
